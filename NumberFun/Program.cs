e799ef2342532c4a3bfcb6b2dd368c32a8d7872e
Xavier Dlask <Xavier.Dlask@education.nsw.gov.au>
2017-10-18 09:59:58 +1100

    Completed Number Fun

---
+++ NumberFun/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number 1 :");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number 2 :");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} plus {num2} = {(num1 + num2)}");
+            Console.WriteLine($"{num1} minus {num2} = {(num1 - num2)}");
+            Console.WriteLine($"{num1} times {num2} = {(num1 * num2)}");
+            Console.WriteLine($"{num1} divided by {num2} = {(num1 / num2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
