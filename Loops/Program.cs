34a2786c36328ef1914e9ad00b9bd292267bb309
Xavier Dlask <Xavier.Dlask@education.nsw.gov.au>
2017-10-24 14:51:41 +1100

    Completed Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int num = 1; num <= number; num++)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
